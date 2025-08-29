9762154aa0fa0aa4898fabc1fe9c8a863c672012
skpatel7013 <skprobo1138@gmail.com>
2025-08-29 18:50:52 +0530

    publisher-subscriber

---
+++ ros2_project/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/skpro/Desktop/Robotics Learning/ros2_project/src/cpp_pubsub
+# Build directory: /home/skpro/Desktop/Robotics Learning/ros2_project/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
